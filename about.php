9f91ddd400df57edf7df77f5bc8b2184fe1a11cd
Eug <187samp@gmail.com>
2022-12-30 21:36:02 +0200

    Added inc folder

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php include_once "./inc/header.php";
+ include_once "./inc/navbar.php";?>
+
+    About Page<br>
+
+    this app is displaying all the products in the store..
+    
+    <?php include_once "./inc/footer.php" ?>
--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/catalog.css">
-    <title>The Catalog</title>
-</head>
-<body class="bg-dark text-white">
-    
-
 <!-- /*
 variables:
     store_name
@@ -43,27 +30,8 @@ variables:
 //     print_r($arr1);
 //     -->
 
-<?php ?>
-
-<header><nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">The Catalog</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">About</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-light" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav></header>
+<?php include_once "./inc/header.php";
+ include_once "./inc/navbar.php";?>
 
 <main>
     <div class="text-center">
@@ -77,7 +45,4 @@ variables:
     <h3>Our special offers</h3>
 </main>
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include_once "./inc/footer.php" ?>
