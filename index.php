ab9f9c7032bb18125978e28c8e4c47cf01c2ac09
Eug <187samp@gmail.com>
2022-12-31 16:22:28 +0200

    cards print with foreach loop

--- index.php
+++ index.php
@@ -41,6 +41,53 @@ variables:
 
     <h3>Trending products</h3>
 
+<div class="row mb-5 pb-3">
+
+<?php
+
+$products = [
+  [
+  'name' => 'Mike running shoes',
+  'description' => 'Good shoes',
+  'price' => '$54.00',
+  'image' => "https://cdn.pixabay.com/photo/2014/05/18/11/26/shoes-346986__340.jpg",
+  'rating' => 5,
+  ],
+  [
+    'name' => 'Tennis edge',
+    'description' => 'Tennis gear',
+    'price' => '$38.00',
+    'image' => "https://cdn.pixabay.com/photo/2021/06/04/06/54/racket-6308994__340.jpg",
+    'rating' => 4,
+    ],
+    [
+      'name' => 'Weight it',
+      'description' => 'Weights',
+      'price' => '$108.00',
+      'image' => "https://cdn.pixabay.com/photo/2016/08/31/22/20/weights-1634747__340.jpg",
+      'rating' => 5,
+    ],
+];
+
+$len = count($products);
+
+// for($i=0; $i < $len; $i++){
+  foreach($products as $prod){
+  echo "<div class=\"col\">";
+  echo "<div class=\"card\">";
+  echo "<img src=\"{$prod['image']}\" class=\"card-img-top\" alt=\"...\">";
+  echo "<div class=\"card-body text-dark\">";
+  echo "<h5 class=\"card-title\">{$prod['name']}</h5>";
+  echo "<p class=\"card-text\">{$prod['price']}</p>";
+  echo "<p class=\"card-text\">{$prod['description']}</p>";
+  echo "<p class=\"card-text\">{$prod['rating']}</p>";
+  echo "<a href=\"#\" class=\"btn btn-primary\">View product</a>";
+  echo "</div>";
+  echo "</div>";
+  echo "</div>";
+} ?>
+
+
 
     <h3>Our special offers</h3>
 </main>
