820a410fa7ab9cc93b9b88c931f6fc5381149f3e
Eug <187samp@gmail.com>
2023-01-09 12:33:10 +0200

    Added upload feature and require

--- about.php
+++ about.php
@@ -1,8 +1,16 @@
-<?php include_once "./inc/header.php";
- include_once "./inc/navbar.php";?>
+<?php
+include_once "./inc/header.php";
+include_once "./inc/navbar.php";
+?>
 
-    About Page<br>
+<main class="p-4 bg-dark text-white shadow">
 
-    this app is displaying all the products in the store..
-    
-    <?php include_once "./inc/footer.php" ?>
+    <div class="text-center">
+        <h2>About Sport Superstar</h2>
+        <h5>What a store!</h5>
+    </div>
+
+</main>
+
+<?php
+include_once "./inc/footer.php";
--- index.php
+++ index.php
@@ -1,95 +1,117 @@
-<!-- /*
-variables:
-    store_name
-    last_update
-    product_name
-    product_price
-*/
-
-// $store_name = "Apple";
-// $last_update = '12/2022';
-// $product_name = "iPad";
-// $pdocut_price = 400;
-
-
-//     $var1 = 10; //value define
-//     $str1 = 'aaa';
-//     $str2= "bbb";
-//     $bool1 = true;
-//     $empty = null;
-//     $arr1 = [1,2,3];
-
-//     define("CONSTANT1", "aaaaaa"); //const value
-
-//     echo CONSTANT1;
-
-//     // var_dump($str2);
-//     // var_dump($var1);
-//     // var_dump($arr1);
-//     echo "<br>";
-//     print_r($arr1);
-//     -->
-
-<?php include_once "./inc/header.php";
- include_once "./inc/navbar.php";?>
-
-<main>
-    <div class="text-center">
-    <h2>Sport Superstar</h2>
-    <h5>Everything that you need to stay active</h5>
-</div>
-
-    <h3>Trending products</h3>
-
-<div class="row mb-5 pb-3">
-
 <?php
+require_once "./inc/utils.php";
+include_once "./inc/header.php";
+include_once "./inc/navbar.php";
+?>
 
-$products = [
-  [
-  'name' => 'Mike running shoes',
-  'description' => 'Good shoes',
-  'price' => '$54.00',
-  'image' => "https://cdn.pixabay.com/photo/2014/05/18/11/26/shoes-346986__340.jpg",
-  'rating' => 5,
-  ],
-  [
-    'name' => 'Tennis edge',
-    'description' => 'Tennis gear',
-    'price' => '$38.00',
-    'image' => "https://cdn.pixabay.com/photo/2021/06/04/06/54/racket-6308994__340.jpg",
-    'rating' => 4,
-    ],
-    [
-      'name' => 'Weight it',
-      'description' => 'Weights',
-      'price' => '$108.00',
-      'image' => "https://cdn.pixabay.com/photo/2016/08/31/22/20/weights-1634747__340.jpg",
-      'rating' => 5,
-    ],
-];
+<main class="p-4 bg-dark text-white shadow">
+    <div class="text-center">
+        <h2>Sport Superstar</h2>
+        <h5>Everything that you need to stay active</h5>
+    </div>
 
-$len = count($products);
+    <h5>Trending Products</h5>
 
-// for($i=0; $i < $len; $i++){
-  foreach($products as $prod){
-  echo "<div class=\"col-sm-12 col-md-4 mb-3\">";
-  echo "<div class=\"card\">";
-  echo "<img src=\"{$prod['image']}\" class=\"card-img-top\" alt=\"...\">";
-  echo "<div class=\"card-body text-dark\">";
-  echo "<h5 class=\"card-title\">{$prod['name']}</h5>";
-  echo "<p class=\"card-text\">{$prod['price']}</p>";
-  echo "<p class=\"card-text\">{$prod['description']}</p>";
-  echo "<p class=\"card-text\">{$prod['rating']}</p>";
-  echo "<a href=\"#\" class=\"btn btn-primary\">View product</a>";
-  echo "</div>";
-  echo "</div>";
-  echo "</div>";
-} ?>
+    <div class="row mb-5 pb-3">
 
+        <?php
 
+        $products = [
+            [
+                'name' => 'Mike Running Shoes',
+                'description' => 'Very nice product',
+                'price' => 54,
+                'image' => 'https://cdn.pixabay.com/photo/2014/05/18/11/26/shoes-346986__340.jpg',
+                'rating' => 5,
+            ],
+            [
+                'name' => 'Tennis Edge',
+                'description' => 'Description here...',
+                'price' => 38,
+                'image' => 'https://cdn.pixabay.com/photo/2021/06/04/06/54/racket-6308994__340.jpg',
+                'rating' => 4,
+            ],
+            [
+                'name' => 'Weight It',
+                'description' => 'Mmm... expansive!',
+                'price' => 108,
+                'image' => 'https://cdn.pixabay.com/photo/2016/08/31/22/20/weights-1634747__340.jpg',
+                'rating' => 5,
+            ],
+        ];
 
-    <h3>Our special offers</h3>
+        $recommended = ['Tennis Edge', 'Weight It'];
+
+        $fn_icon = function ($name) {
+            global $recommended;
+            return in_array($name, $recommended) ? "bi-star-fill" : "bi-star";
+        };
+
+        // $len = count($products);
+        foreach ($products as $item) {
+            $col = <<<COL
+                <div class="col-sm-12 col-md-4 mb-3">
+                    <div class="card">
+                        <img src="{$item['image']}" class="card-img-top" alt="{$item['name']}">
+                        <div class="card-body text-dark">
+                            <h5 class="card-title">{$item['name']}</h5>
+                            <p class="card-text">{$fn_price($item['price'])}</p> 
+                            <p class="card-text">{$item['description']}</p>
+                            <p class="card-text">rating: {$item['rating']}</p>
+                            <p class="card-text"><i class="{$fn_icon($item['name'])}"></i></p>
+                            <a href="product.php?prod={$item['name']}" class="btn btn-primary">View Product</a>
+                        </div>
+                    </div>
+                </div>
+            COL;
+
+            echo $col;
+        }
+        ?>
+    </div>
+
+    <?php $time = time(); ?>
+
+    <h5>
+        Our Special Offers for <?= date('d/m/Y', $time) ?>
+    </h5>
+
+    <?php
+    $offers = [
+        [
+            'title' => 'Black Friday Sale',
+            'description' => 'buy now before the offer expires',
+            'code' => 'BF015'
+        ],
+        [
+            'title' => '2 in the price of 1',
+            'description' => 'for club members only!',
+            'code' => 'TO211'
+        ]
+    ];
+    ?>
+
+    <div class="row mb-5 pb-3">
+        <?php
+        foreach ($offers as $offer) {
+            $card = <<<CARD
+                <div class="col-sm-12 col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-body text-dark">
+                            <h5 class="card-title">{$offer['title']}</h5>
+                            <p class="card-text">{$offer['description']}</p>
+                            <a href="offer.php?code={$offer['code']}&title={$offer['title']}" class="btn btn-primary">Get It</a>
+                        </div>
+                    </div>
+                </div>
+            CARD;
+
+            echo $card;
+        }
+        ?>
+    </div>
+
 </main>
 
-<?php include_once "./inc/footer.php" ?>
+<?php
+include_once "./inc/footer.php";
